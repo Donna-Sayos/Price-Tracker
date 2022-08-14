1d85dbb266839bf1a83f27cd07940684e8047dca
Donna-Sayos <Donnasayos@gmail.com>
2022-08-14 18:25:30 -0400

    amazon parser DONE!!

--- amazonParser.js
+++ amazonParser.js
@@ -1,69 +1,49 @@
-require("dotenv").config(); // this loads all of our environment variables into the process.env variable;
-
 const nightmare = require("nightmare")();
 // you have to require nightmare. Nighmare returns a function;
 // remove all the extra information after the path in the url you're telling nightmare to go to;
 
-const sendGridMail = require("@sendgrid/mail");
-sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
+const nodemailer = require("nodemailer");
 
 const cron = require('node-cron'); // schedule tasks to be run on the server at a scheduled time;
 
-// console.log(process.argv)
-// process.exit(0)
+// console.log(process.argv);
+// process.exit(0);
 const args = process.argv.slice(2);
-const url1 = args[0];
-const minPrice1 = args[1];
+const url = args[0];
+const minPrice = args[1];
 
-const url2 = args[2];
-const minPrice2 = args[3];
-
-checkPrice();
-async function checkPrice() {
+cron.schedule('* * * * *',async function checkPrice() {
     console.log('running every minute...')
     try {
-      const item1 = await nightmare
-        .goto(url1)
+      const item = await nightmare
+        .goto(url)
         .wait(".a-price-whole")
         .evaluate(() => document.querySelector(".a-price-whole").innerText)
         .end();
 
-      // const item2 = await nightmare
-      //   .goto(url2)
-      //   .wait(".a-price-whole")
-      //   .evaluate(() => document.querySelector(".a-price-whole").innerText)
-      //   .end();
-  
-      const item1Price = Number(item1);
-      // const item2Price = Number(item2);
-      
-      // console.log(item1Price)
-      // console.log(item2Price)
-      if (item1Price < minPrice1) {
-        await sendEmail1();
-      };
+      const itemPrice = Number(item);
 
-      // if (item2Price < minPrice2) {
-      //   await sendEmail2();
-      // }
+      if (itemPrice < minPrice) {
+        await sendEmail();
+      };
     } catch (e) {
-      await sendEmail1("There's an error in your Amazon price checker! 😱", e);
+      await errorEmail(e);
       throw e
     }
-};
+});
 
 const obj = {
   subject: "The price is low!",
+  errorSub: "ERROR!",
   heading: "Hello daddy!",
-  line1: `The price of the item on ${url1} has dropped below $ ${minPrice1} 😱!`,
-  line: `The price of the item on ${url2} has dropped below $ ${minPrice2} 😱!`,
+  line1: `The price of the item on ${url} has dropped below $ ${minPrice} 😱!`,
   line2: "Better go and buy it as soon as you can before the price goes back to regular.",
   line3: "Please enjoy this photo of a corgi's butt.",
   imageUrl: "https://i.chzbgr.com/full/9441876224/h39F6DD50/wheel",
   endNote: "Yours truly -- 💋💋",
 };
 
-let htmlTemplate = [`
+let htmlTemplate = `
       <!DOCTYPE html>
       <html>
       <body>
@@ -78,58 +58,54 @@ let htmlTemplate = [`
         <h4 style="text-align:center">${obj.endNote}</h4>
       </body>
       </html>
-`, `
-      <!DOCTYPE html>
-      <html>
-      <body>
-        <h1>${obj.heading}</h1>
+`;
 
-        <p>${obj.line}</p>
-        <p>${obj.line2}</p>
-        <p>${obj.line3}</p>
+async function sendEmail() {
+  const user = "donnasayos@gmail.com";
+  const pass = "wbveycvcertlhnaa"; // not my actual password. It won't let you login to my gmail even if you tried lol;
 
-        <img src=${obj.imageUrl} alt="corgu_butt" />
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // smtp is the main transport in nodemailer; Simple Mail Transport Protocol -- is used for sending out emails;
+    port: 587, // default port nodemailer uses; 465 if you want to do bulk mailing;
+    secure: false, // true for port 465, false for other ports; nodemailer uses TLS - Transport Layer Security (for privacy and data security for communications over the internet);
+    auth: { // is what allows nodemailer to use my gmail to send out emails;
+      user,
+      pass 
+    },
+  });
 
-        <h4 style="text-align:center">${obj.endNote}</h4>
-      </body>
-      </html>
-`];
-
-function sendEmail1() {
-  const email = {
-      to: {
-        email: "donnasayos@gmail.com",
-        name: "Donna Sayos",
-      },
-      subject: `${obj.subject}`,
-      from: {
-        email: "donnasayos@gmail.com",
-        name: "Amazon Price Tracker",
-      },
-      content: [{ type: "text/html", value: htmlTemplate[0] }]
-  };
-
-  console.log(email);
-  return sendGridMail.send(email);
+  await transporter.sendMail({
+    from: `"Your Amazon Price Tracker" <${user}>`, // sender address
+    to: "sugarzaddy1234@gmail.com", // list of receivers
+    subject: `${obj.subject}`, // Subject line
+    // text: "Hello world?", // plain text body
+    html: htmlTemplate, // html body
+  });
+  console.log("Email sent...💌")
 };
 
-// function sendEmail2() {
-//   const email = {
-//     to: {
-//       email: "donnasayos@gmail.com",
-//       name: "Donna Sayos",
-//     },
-//     subject: `${obj.subject}`,
-//     from: {
-//       email: "donnasayos@gmail.com",
-//       name: "Amazon Price Tracker",
-//     },
-//     content: [{ type: "text/html", value: htmlTemplate[1] }]
-//   };
+async function errorEmail() {
+  const user = "donnasayos@gmail.com";
+  const pass = "wbveycvcertlhnaa";
 
-//   console.log(email);
-//   return sendGridMail.send(email);
-// };
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587, 
+    secure: false,
+    auth: {
+      user,
+      pass 
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Your Amazon Price Tracker" <${user}>`,
+    to: "sugarzaddy1234@gmail.com", 
+    subject: `${obj.errorSub}`, 
+    text: "There's an error in your Amazon price checker! 😱"
+  });
+  console.log("Error email sent...💌")
+};
 
 /* 
 1.  we go to the url;
@@ -139,6 +115,4 @@ function sendEmail1() {
 
 /*
   everyday at midnight -->  0 0 * * *
-*/
-
-// cron.schedule('* * * * *', 
+*/ 
